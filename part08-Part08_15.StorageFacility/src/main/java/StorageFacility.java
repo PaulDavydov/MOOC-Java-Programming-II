f22a866b5ecca6bc305887ea0195677937c5f2b0
Paul Davydov <sirruskilotpaul@gmail.com>
2021-01-13 02:12:10 -0800

    Moving on to Part 9

--- part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
+++ part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class StorageFacility {
-    
+    private HashMap<String, ArrayList<String>> facility;
+    public StorageFacility() {
+        facility = new HashMap<>();
+    }
+    public void add(String unit, String item) {
+        this.facility.putIfAbsent(unit,new ArrayList<>());
+        ArrayList<String> items = this.facility.get(unit);
+        items.add(item);
+    }
+    public ArrayList<String> contents(String storageUnit) {
+        if(facility.containsKey(storageUnit)) {
+            return facility.get(storageUnit);
+        }
+        return new ArrayList<String>();
+    }
+    public void remove(String storageUnit, String item) {
+        facility.get(storageUnit).remove(item);
+        if (facility.get(storageUnit).isEmpty()) {
+            facility.remove(storageUnit);
+        }
+    }
+    public ArrayList<String> storageUnits() {
+        ArrayList<String> names = new ArrayList<>();
+        for ( String i : facility.keySet()) {
+            if(facility.get(i).isEmpty()){
+                continue;
+            }
+            names.add(i);
+        }
+        return names;
+    }
+
 }
