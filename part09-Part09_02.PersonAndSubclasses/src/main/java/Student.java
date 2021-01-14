de9ab0bbe20f6d66cad96372c4a0003f1b627d93
Paul Davydov <sirruskilotpaul@gmail.com>
2021-01-14 02:04:09 -0800

    Finished Part 9 questions 1-2
    Working this late is not a great idea tbh

---
+++ part09-Part09_02.PersonAndSubclasses/src/main/java/Student.java
@@ -0,0 +1,17 @@
+public class Student extends Person {
+    private int credits;
+
+    public Student(String name, String address) {
+        super(name, address);
+        this.credits = 0;
+    }
+    public int credits() {
+        return this.credits;
+    }
+    public void study() {
+        credits++;
+    }
+    public String toString() {
+        return getName() + "\n  " + getAddress() + "\n  "+ "Study credits " +this.credits;
+    }
+}
