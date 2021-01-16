1fcb1fc9398a0c7eab7e5867f82cf96f82fc982b
Paul Davydov <sirruskilotpaul@gmail.com>
2021-01-15 16:03:32 -0800

    Started and finished TacoBoxes
    pretty easy problem

---
+++ part09-Part09_05.TacoBoxes/src/main/java/CustomTacoBox.java
@@ -0,0 +1,15 @@
+public class CustomTacoBox implements TacoBox{
+    private int tacos;
+    public CustomTacoBox (int tacos) {
+        this.tacos = tacos;
+    }
+    @Override
+    public int tacosRemaining() {
+        return tacos;
+    }
+
+    @Override
+    public void eat() {
+        this.tacos-=1;
+    }
+}
---
+++ part09-Part09_05.TacoBoxes/src/main/java/TripleTacoBox.java
@@ -0,0 +1,17 @@
+public class TripleTacoBox implements TacoBox{
+    private int taco;
+    public TripleTacoBox() {
+        this.taco = 3;
+    }
+
+
+    @Override
+    public int tacosRemaining() {
+        return taco;
+    }
+
+    @Override
+    public void eat() {
+        taco -= 1;
+    }
+}
