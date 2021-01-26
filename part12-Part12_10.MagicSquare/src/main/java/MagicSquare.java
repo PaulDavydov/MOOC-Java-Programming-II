19b30ae1f1cdecc6ac7c77b18db8f073724a95cf
Paul Davydov <sirruskilotpaul@gmail.com>
2021-01-25 18:42:30 -0800

    Confused on the Siamese Algorithm

--- part12-Part12_10.MagicSquare/src/main/java/MagicSquare.java
+++ part12-Part12_10.MagicSquare/src/main/java/MagicSquare.java
@@ -17,16 +17,49 @@ public class MagicSquare {
 
     // implement these three methods
     public ArrayList<Integer> sumsOfRows() {
-        for 
-        return new ArrayList<>();
+        ArrayList<Integer> sumR = new ArrayList<>();
+        int sum = 0;
+        for (int row = 0; row < square.length; row++) {
+            for (int column = 0; column < square[row].length;column++) {
+                int value = square[row][column];
+                sum+=value;
+            }
+            sumR.add(sum);
+            sum = 0;
+        }
+        return sumR;
     }
 
     public ArrayList<Integer> sumsOfColumns() {
-        return new ArrayList<>();
+        ArrayList<Integer> sumC = new ArrayList<>();
+        for(int col = 0; col < square.length; col++) {
+            int sum = 0;
+            for(int row = 0; row <square[col].length;row++) {
+                int value = square[row][col];
+                sum+=value;
+            }
+            sumC.add(sum);
+        }
+        return sumC;
     }
 
     public ArrayList<Integer> sumsOfDiagonals() {
-        return new ArrayList<>();
+        ArrayList<Integer> sumD = new ArrayList<>();
+        int sum1 = 0;
+        int sum2 = 0;
+        int last = square.length-1;
+        for (int i = 0; i < square.length; i++) {
+            int value = square[i][i];
+            sum1+=value;
+        }
+        sumD.add(sum1);
+        for(int row = last; row >=0;row--) {
+            int value = square[row][last-row];
+            sum2+=value;
+        }
+
+        sumD.add(sum2);
+        return sumD;
     }
 
     // ready-made helper methods -- don't touch these
--- part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
+++ part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
@@ -6,6 +6,32 @@ public class MagicSquareFactory {
         MagicSquare square = new MagicSquare(size);
 
         // implement the creation of a magic square with the Siamese method algorithm here
+        int numbers = size*size;
+        int i = 1;
+        int row = 0;
+        int col = size/2;
+        int curr_Row;
+        int curr_Col;
+        while (i<=numbers) {
+            square.placeValue(row,col,i);
+            curr_Row = row;
+            curr_Col = col;
+            row -= 1;
+            col += 1;
+            if (row == -1) {
+                row = size -1;
+            }
+            if(col == size) {
+                col = 0;
+            }
+            if (square.readValue(row,col) != 0) {
+                row = curr_Row +1;
+                col = curr_Col;
+                if (row == -1) {
+                    row = size - 1;
+                }
+            }
+        }
         return square;
     }
 
