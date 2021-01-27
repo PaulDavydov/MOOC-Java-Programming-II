5e8de5107d12a2cb6048a47bbb8caa5def5d54f6
Paul Davydov <sirruskilotpaul@gmail.com>
2021-01-26 17:01:41 -0800

    Finished MagicSquareFactory

--- part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
+++ part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
@@ -7,28 +7,29 @@ public class MagicSquareFactory {
 
         // implement the creation of a magic square with the Siamese method algorithm here
         int numbers = size*size;
-        int i = 1;
+
         int row = 0;
         int col = size/2;
         int curr_Row;
         int curr_Col;
-        while (i<=numbers) {
-            square.placeValue(row,col,i);
+
+        for (int i =1; i <= numbers; i++) {
+            square.placeValue(col,row,i);
             curr_Row = row;
             curr_Col = col;
-            row -= 1;
-            col += 1;
-            if (row == -1) {
-                row = size -1;
+            row --;
+            col++;
+            if(row == -1) {
+                row = square.getWidth()-1;
             }
-            if(col == size) {
+            if(col == square.getHeight()) {
                 col = 0;
             }
-            if (square.readValue(row,col) != 0) {
+            if (square.readValue(col,row) != 0) {
                 row = curr_Row +1;
                 col = curr_Col;
-                if (row == -1) {
-                    row = size - 1;
+                if(row < 0) {
+                    row = square.getWidth()-1;
                 }
             }
         }
