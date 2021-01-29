5210cf4b52d7072f369f3ca4e0e73818c01b9433
Paul Davydov <sirruskilotpaul@gmail.com>
2021-01-28 23:52:53 -0800

    Finished Part 13 section 2

--- part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,37 @@
 package textstatistics;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class TextStatisticsApplication {
+public class TextStatisticsApplication extends Application{
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane layout = new BorderPane();
+
+        HBox text = new HBox();
+        text.setSpacing(10);
+        text.getChildren().add(new Label("Letters: 0"));
+        text.getChildren().add(new Label("Words: 0"));
+        text.getChildren().add(new Label("The longest word is:"));
+
+        layout.setCenter(new TextArea(""));
+        layout.setBottom(text);
+
+        Scene view = new Scene(layout);
+        stage.setScene(view);
+        stage.show();
+    }
 }
