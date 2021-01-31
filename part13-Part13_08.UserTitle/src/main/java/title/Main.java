04accf40727c207b7afb67976840b883d57bacf6
Paul Davydov <sirruskilotpaul@gmail.com>
2021-01-31 00:06:11 -0800

    One more section left for part 13

--- part13-Part13_08.UserTitle/src/main/java/title/Main.java
+++ part13-Part13_08.UserTitle/src/main/java/title/Main.java
@@ -1,11 +1,16 @@
 package title;
 
+import javafx.application.Application;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        Application.launch(UserTitle.class,
+                "--userTitle=" + word);
     }
 
 }
--- part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
+++ part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
@@ -1,7 +1,20 @@
 package title;
 
+import javafx.application.Application;
+import javafx.application.Application.Parameters;
+import javafx.stage.Stage;
 
-public class UserTitle {
+import java.util.Scanner;
 
+public class UserTitle extends Application {
 
+    @Override
+    public void start(Stage window) {
+        Parameters params = getParameters();
+        String organization = params.getNamed().get("userTitle");
+
+        window.setTitle(organization);
+        window.show();
+
+    }
 }
