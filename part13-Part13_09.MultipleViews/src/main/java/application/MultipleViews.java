a67fbd3366518b84c2b02384a4fa490c05c802fd
Paul Davydov <sirruskilotpaul@gmail.com>
2021-02-01 17:51:44 -0800

    Continuing Part 13

--- part13-Part13_09.MultipleViews/src/main/java/application/MultipleViews.java
+++ part13-Part13_09.MultipleViews/src/main/java/application/MultipleViews.java
@@ -1,9 +1,19 @@
 package application;
 
-public class MultipleViews {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MultipleViews extends Application {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 
+    public void start(Stage window) {
+        BorderPane pane = new BorderPane();
+        
+    }
 }
