a07fcdd62c1ec467397d0a541ba0f46af1a1f4bf
Paul Davydov <sirruskilotpaul@gmail.com>
2021-02-04 01:56:08 -0800

    Continuing to work on Part 14

--- part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
+++ part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
@@ -1,9 +1,73 @@
 package application;
 
-public class PartiesApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+import java.nio.file.Paths;
+import java.util.*;
+
+public class PartiesApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(PartiesApplication.class);
     }
 
+    @Override
+    public void start(Stage stage) {
+        String file = "partiesdata.tsv";
+        try (Scanner scanner = new Scanner(Paths.get(file))) {
+            Map<String,Map<Integer,Double>> values  = new HashMap<>();
+
+            NumberAxis xAxis = new NumberAxis(1968,2008,4);
+            NumberAxis yAxis = new NumberAxis(0,30,5);
+
+            LineChart<Number,Number> lineChart = new LineChart<>(xAxis,yAxis);
+            lineChart.setTitle("Relative support of the parties");
+
+            String row0 = scanner.nextLine();
+            List<String> years = Arrays.asList(row0.split("\t"));
+
+            while(scanner.hasNextLine()) {
+                String row = scanner.nextLine();
+                List<String> pieces = Arrays.asList(row.split("\t"));
+                String partName = pieces.get(0);
+                Map<Integer, Double> partyData = new HashMap<>();
+                for (int i = 1; i < pieces.size();i++) {
+                    if(!pieces.get(i).equals("-")) {
+                        int year = Integer.valueOf(years.get(i));
+                        String dat = pieces.get(i);
+                        double number  = Double.parseDouble(dat);
+                        partyData.put(year, number);
+                    }
+                }
+                values.put(partName,partyData);
+            }
+
+            values.keySet().stream().forEach(party -> {
+                // a different data set for every party
+                XYChart.Series data = new XYChart.Series();
+                data.setName(party);
+
+                // add the party's support numbers to the data set
+                values.get(party).entrySet().stream().forEach(pair -> {
+                    data.getData().add(new XYChart.Data(pair.getKey(), pair.getValue()));
+                });
+
+                // and add the data set to the chart
+                lineChart.getData().add(data);
+            });
+
+            Scene view = new Scene(lineChart,400,300);
+            stage.setScene(view);
+            stage.show();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
--- part14-Part14_03.SavingsCalculator/src/main/java/application/SavingsCalculatorApplication.java
+++ part14-Part14_03.SavingsCalculator/src/main/java/application/SavingsCalculatorApplication.java
@@ -1,9 +1,71 @@
 package application;
 
-public class SavingsCalculatorApplication {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class SavingsCalculatorApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(SavingsCalculatorApplication.class);
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane pane = new BorderPane();
+
+        NumberAxis xAxis = new NumberAxis(0,20,1);
+        NumberAxis yAxis = new NumberAxis(0,20000,2500);
+        LineChart chart = getChart(xAxis,yAxis);
+
+        VBox vBox = new VBox(2);
+        BorderPane sub1 = new BorderPane();
+        BorderPane sub2 = new BorderPane();
+        Label label1 = new Label("Monthly savings");
+        Label label2 = new Label("Yearly interest rate");
+        Slider slide1 = new Slider(25,250,25);
+        Slider slide2 = new Slider(0,10,1);
+
+        sub1.getChildren().addAll(label1,slide1);
+        sub2.getChildren().addAll(label2,slide2);
+        vBox.getChildren().add(sub2);
+        vBox.setAlignment(Pos.CENTER);
+        pane.setTop(vBox);
+        BorderPane.setAlignment(vBox,Pos.CENTER);
+        //pane.setCenter(chart);
+
+
+
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private BorderPane getPane() {
+        BorderPane pane = new BorderPane();
+        return pane;
+    }
+
+    private LineChart<Number,Number> getChart (NumberAxis x, NumberAxis y) {
+        LineChart<Number,Number> chart = new LineChart<>(x,y);
+        chart.setTitle("Saving");
+        return chart;
+    }
+
+    private Slider getSlider(int x, int y, double z) {
+        Slider slider = new Slider(x,y,z);
+        slider.setShowTickMarks(true);
+        slider.setShowTickLabels(true);
+        return slider;
+    }
+
+
 
 }
